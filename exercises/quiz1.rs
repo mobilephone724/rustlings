a4f40497e169d266f58b60eae8e6d165fdb07ad3
mobile724.yang <mobile724.yang@foxmail.com>
2024-05-27 14:23:21 +0000

    00-05 are finished

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (cur_price: i32) -> i32 {
+    if cur_price <= 40 {
+        cur_price * 2
+    } else {
+        cur_price
+    }
+}
 
 // Don't modify this function!
 #[test]
